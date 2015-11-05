7e8dc27ae1fdbacbccb6f4f8513e776418a3907f
Sven Akkermans <sven.akkermans@cs.kuleuven.be>
2015-11-05 12:31:59 +0100

    keeping watchdog implementation, possible useful for future

--- bsp/boards/zigduino/watchdog_timer.c
+++ bsp/boards/zigduino/watchdog_timer.c
@@ -8,17 +8,7 @@
 #include "board.h"
 #include "board_info.h"
 
-#include <avr/pgmspace.h>
-#include <avr/fuse.h>
-#include <avr/eeprom.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
-
-#include <avr/iom128rfa1.h> //Sven: this is advised against, but works.
-#include <avr/sleep.h>
-#include <avr/power.h>
 #include <avr/wdt.h>
-#include <avr/interrupt.h>
 
 
 //=========================== define ==========================================
@@ -66,37 +56,6 @@ void bsp_timer_init(){
 
 	MCUSR &=~(1<<WDRF);	//  Clear startup bit and disable the wdt, whether or not it will be used.
 	WDTCSR = (0<<WDIE);	// Disable watchdog timeout interrupt enable
-
-	//	PRR0 &= ~(1<<PRTIM0); // turn on timer 2 for crystal
-	//
-	//	/* Disable TIMER2 interrupt */
-	//	TIMSK2 &= ~(1 << OCIE2A);
-	//	ASSR = 0x00;
-	//	TCNT2 = 0;	// Set counter to zero
-	//	while(ASSR & (1<<TCN2UB)) {};
-	//
-	//	TCCR2A = (0<<COM2A1) | (0<<COM2A0) | (0<<COM2B1) | (0<<COM2B0) | (1<<WGM21) | (0<<WGM20);
-	//	TCCR2B = (0<<WGM22) |(1<<CS22)|(1<<CS21)|(1<<CS20); //prescale: 128 for timer2
-	//	while(ASSR & (1<<TCR2BUB)) {}; // check if busy
-
-	//	TCNT0 = 0;
-	//	TCCR0A = (1 << WGM01);
-	//	TCCR0B =  (1<<CS02)|(0<<CS01)|(1<<CS00); //prescale: 1024 for timer0
-	//	/* Clear interrupt flag register */ \
-	//	TIFR0 = TIFR0; \
-
-	//	PRR0 &= ~(1<<PRTIM2); // turn on timer 2 for crystal
-	//	SCCR0 = (SCCR0 | 0b00110000) & 0b11111110; // enable symbol counter, 32KHz clock, absolute compare 1
-	//	SCCR1 = 0; // no backoff slot counter
-	//	ASSR |= (1<<AS2); // enable 32KHz crystal
-	//
-	//	//set compare1 registers
-	//	SCOCR1HH = SCOCR1HL = SCOCR1LH = SCOCR1LL = 0;
-	//
-	//	// don't enable interrupts until first compare is set
-	//
-	//	// wait for register writes
-	//	while(SCSR & 0x01);
 }
 
 /**
@@ -168,49 +127,6 @@ void bsp_timer_scheduleIn(PORT_TIMER_WIDTH delayTicks){
 		wdt_enable_int_only(wdt_count); //Macro sets things up for a reset
 		WDTCSR |= (1 << WDIE);
 	}
-	//	PORT_TIMER_WIDTH newCompareValue;
-	//	PORT_TIMER_WIDTH temp_last_compare_value;
-	//	PORT_TIMER_WIDTH current_value;
-	//
-	//	temp_last_compare_value = bsp_timer_vars.last_compare_value;
-	//
-	//	newCompareValue      =  bsp_timer_vars.last_compare_value + delayTicks;
-	//	bsp_timer_vars.last_compare_value   =  newCompareValue;
-	//
-	//	current_value = bsp_timer_get_currentValue();
-	//
-	//	printf("\n\n");
-	//	printf("delayTicks: %lu\n", delayTicks);
-	//	printf("current value: %lu\n", current_value);
-	//	printf("newCompareValue: %lu\n", newCompareValue);
-	//	printf("temp_last_compare_value: %lu\n", temp_last_compare_value);
-	//	PORT_TIMER_WIDTH passed_time = current_value - temp_last_compare_value;
-	//	printf("passed_time: %lu\n",passed_time);
-	//
-	//	if (current_value > temp_last_compare_value && delayTicks < current_value - temp_last_compare_value) {
-	//		// we're already too late, schedule the ISR right now manually
-	//		// setting the interrupt flag triggers an interrupt
-	//		printf("Too late, triggered interrupt \n");
-	//		bsp_timer_isr();
-	//
-	//	} else {
-	//		printf("Set compare, enabled interrupt\n");
-	//		//		OCR2A  =  newCompareValue;
-	//		//		TIMSK2 |= (1 << OCIE2A); // Enable TIMER2 output compare interrupt
-	//		//		//  while(ASSR & (1<<OCR2AUB));
-	//
-	//		//		OCR0A  =  newCompareValue;
-	//		//		TIMSK0 |= (1 << OCIE0A); // Enable TIMER0 output compare interrupt
-	//
-	//		// this is the normal case, have timer expire at newCompareValue
-	//		SCOCR1HH  =  (uint8_t)(newCompareValue>>24);
-	//		SCOCR1HL  =  (uint8_t)(newCompareValue>>16);
-	//		SCOCR1LH  =  (uint8_t)(newCompareValue>>8);
-	//		SCOCR1LL  =  (uint8_t)(newCompareValue);
-	//		// enable interrupts
-	//		SCIRQM |= 0x01;
-	//
-	//	}
 }
 
 /**

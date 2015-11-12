f27a98cff13ca7e2d71dabce854f8ae6781367b6
Sven Akkermans <sven.akkermans@cs.kuleuven.be>
2015-11-12 08:39:33 +0100

    added rng, generate random eui64 now

--- bsp/boards/zigduino/eui64.c
+++ bsp/boards/zigduino/eui64.c
@@ -2,7 +2,7 @@
 \brief Zigduino definition of the "eui64" bsp module.
 
 \author Sven Akkermans <sven.akkermans@cs.kuleuven.be>, September 2015.
-*/
+ */
 
 #include "string.h"
 #include "eui64.h"
@@ -18,32 +18,38 @@
 //=========================== prototypes ======================================
 uint8_t
 rng_get_uint8(void) {
-	  uint8_t i,j;
-	  ADCSRA=1<<ADEN;             //Enable ADC, not free running, interrupt disabled, fastest clock
-	  for (i=0;i<4;i++) {
-	    ADMUX = 0;                //toggle reference to increase noise
-	    ADMUX =0x1E;              //Select AREF as reference, measure 1.1 volt bandgap reference.
-	    ADCSRA|=1<<ADSC;          //Start conversion
-	    while (ADCSRA&(1<<ADSC)); //Wait till done
-		j = (j<<2) + ADC;
-	  }
-	  ADCSRA=0;                   //Disable ADC
-	   printf("rng issues %u\n",j);
-	  return j;}
+
+	uint8_t temp = TRX_STATE;
+	TRX_STATE = (TRX_STATE & 0xE0) | RX_ON; // Ensure radio can generate rnd values.
+
+	uint8_t j = 0;
+    uint8_t i = 0;
+    for(; i < 4; i ++){
+        j = j * 4;
+        j += ((PHY_RSSI&0x60)>>5);
+       uint32_t delay = 1000;
+        while(delay-->0){}; //introduce short delay to improve randomness.
+    }
+	printf("rng issues %u\n",j);
+
+	TRX_STATE = temp; //Restore radio to previous state
+
+	return j;
+}
 //=========================== public ==========================================
 
 void eui64_get(uint8_t* addressToWrite) {
 	uint8_t rng_id1 = rng_get_uint8();
 	uint8_t rng_id2 = rng_get_uint8();
 
-    addressToWrite[0] = 0x14;   // OpenWSN OUI
-    addressToWrite[1] = 0x15;
-    addressToWrite[2] = 0x92;
-    addressToWrite[3] = 0xe3;
-    addressToWrite[4] = 0xff; // ffff is zigduino
-    addressToWrite[5] = 0xff;
-    addressToWrite[6] = rng_id1;
-    addressToWrite[7] = rng_id2;
+	addressToWrite[0] = 0x14;   // OpenWSN OUI
+	addressToWrite[1] = 0x15;
+	addressToWrite[2] = 0x92;
+	addressToWrite[3] = 0xe3;
+	addressToWrite[4] = 0xff; // ffff is zigduino
+	addressToWrite[5] = 0xff;
+	addressToWrite[6] = rng_id1;
+	addressToWrite[7] = rng_id2;
 
 }
 //=========================== private =========================================

0081c32c681d6876eb0817df6d721292a9b6bc76
Sven Akkermans <sven.akkermans@cs.kuleuven.be>
2015-11-17 11:28:07 +0100

    zigduino generates new address only if necessary, generated ones get stored

--- bsp/boards/zigduino/eui64.c
+++ bsp/boards/zigduino/eui64.c
@@ -14,6 +14,7 @@
 
 //=========================== defines =========================================
 
+#define EUI64_RANDOM_NB_ADDRESS 0x10ee
 //=========================== variables =======================================
 
 //=========================== prototypes ======================================
@@ -21,15 +22,15 @@ uint8_t
 rng_get_uint8(void) {
 	uint8_t temp = TRX_STATE;
 	TRX_STATE = (TRX_STATE & 0xE0) | RX_ON; // Ensure radio can generate rnd values.
-    _delay_us(200);
+	_delay_us(200);
 
 	uint8_t j = 0;
-    uint8_t i = 0;
-    for(; i < 4; i ++){
-        _delay_us(2);
-        j = j * 4;
-        j += ((PHY_RSSI&0x60)>>5);
-    }
+	uint8_t i = 0;
+	for(; i < 4; i ++){
+		_delay_us(2);
+		j = j * 4;
+		j += ((PHY_RSSI&0x60)>>5);
+	}
 	printf("rng issues %u\n",j);
 
 	TRX_STATE = temp; //Restore radio to previous state
@@ -39,47 +40,6 @@ rng_get_uint8(void) {
 //=========================== public ==========================================
 
 void eui64_get(uint8_t* addressToWrite) {
-	uint8_t rng_id1 = rng_get_uint8();
-	uint8_t rng_id2 = rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-	rng_get_uint8();
-
-
-
 
 	addressToWrite[0] = 0x14;   // OpenWSN OUI
 	addressToWrite[1] = 0x15;
@@ -87,9 +47,23 @@ void eui64_get(uint8_t* addressToWrite) {
 	addressToWrite[3] = 0xe3;
 	addressToWrite[4] = 0xff; // ffff is zigduino
 	addressToWrite[5] = 0xff;
+
+	uint8_t rng_id1;
+	uint8_t rng_id2;
+
+	if(eeprom_read_byte(EUI64_RANDOM_NB_ADDRESS) == 0){
+		rng_id1 = rng_get_uint8();
+		rng_id2 = rng_get_uint8();
+
+		eeprom_write_byte(EUI64_RANDOM_NB_ADDRESS,rng_id1);
+		eeprom_write_byte(EUI64_RANDOM_NB_ADDRESS+0x04,rng_id2);
+
+	} else {
+		rng_id1 = eeprom_read_byte(EUI64_RANDOM_NB_ADDRESS);
+		rng_id2 = eeprom_read_byte(EUI64_RANDOM_NB_ADDRESS+0x04);
+	}
+
 	addressToWrite[6] = rng_id1;
 	addressToWrite[7] = rng_id2;
-
 }
 //=========================== private =========================================
-
